d92036efcbaa2a6f66c8ddfad7c21f79178c2e59
Abdo-hassan97 <abdelrahman.younis97@gmail.com>
2022-07-10 17:45:57 +0200

    version one by abdo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import{CrudService} from './service/crud.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'talabat';
+
+  usrFormGroup: FormGroup;
+  Resturant :any
+  ResturantName :any
+  ResturantAddress :any
+  _crudservices : any;
+
+  constructor(crudservices:CrudService ,private fb: FormBuilder){
+    this._crudservices=crudservices;
+    this.usrFormGroup=this.fb.group({
+      ResturantName:[''],
+      ResturantAddress:[''],
+      menus:fb.array([fb.control('')]),
+
+
+     })
+  }
+
+
+
+  get menus(){
+    // console.log(this.usrFormGroup.get('menus') )
+    return this.usrFormGroup.get('menus') as FormArray;
+  }
+  addMobile(){
+    this.menus.push(this.fb.control(''));
+  }
+
+  createRecord(){
+    debugger
+    console.log(this.usrFormGroup)
+let Record:any = {}
+Record['name'] = this.usrFormGroup.controls["ResturantName"].value
+Record['adress'] = this.usrFormGroup.controls["ResturantAddress"].value
+Record['meals'] = this.usrFormGroup.controls["menus"].value
+
+this._crudservices.creatNewResturant(Record).then((res: any) =>{
+  this.ResturantName="";
+  this.ResturantAddress=""
+  console.log(res)
+})
+
+  }
+
 }
